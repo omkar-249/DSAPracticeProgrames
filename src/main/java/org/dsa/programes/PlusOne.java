8803cc41ba40522e78c6b5316f3299e6304ea5e7
omkar-249 <omkarlaxmanbhosale3@gmail.com>
2026-01-12 02:33:03 +0530

    solution for plusone

---
+++ src/main/java/org/dsa/programes/PlusOne.java
@@ -0,0 +1,29 @@
+package org.dsa.programes;
+
+import java.util.Arrays;
+
+public class PlusOne {
+
+    static void main() {
+        System.out.println(Arrays.toString(plusOne(new int[]{9, 9, 9})));
+    }
+    public static int[] plusOne(int[] digits) {
+
+        return getResponseArray(digits, digits.length-1);
+    }
+
+    private static int[] getResponseArray(int[] digits, int k){
+        if(digits[k] <= 8 && k >= 0){
+            digits[k] = digits[k]+1;
+            return digits;
+        }else if(digits[k] == 9 && k >= 1){
+            digits[k] = 0;
+            return getResponseArray(digits, --k);
+        }else if(digits[k] == 9 && k == 0){
+            int[] res = new int[digits.length+1];
+            System.arraycopy(digits, 0, res, 1, digits.length);
+            return getResponseArray(res, ++k);
+        }
+        return digits;
+    }
+}
